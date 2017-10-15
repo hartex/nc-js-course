a47bb3a8dfc26fa335cf223d932e0d47f57520c7
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2017-10-15 23:32:31 +0300

    start working on lecture 1

---
+++ lecture-1/main.js
@@ -0,0 +1,24 @@
+/**
+ * Statements and expression
+ * */
+// statement; statement;
+
+/**
+ * Variable declaration and initialization
+ * */
+
+var x = 1;
+
+let y = 2;
+
+const z = 3;
+
+/**
+ * Writing something to console
+ *
+ * @see {@link https://developer.mozilla.org/ru/docs/Web/API/Console|MDN Console}
+ * */
+let zz = 1;
+console.log("Hello World!");
+
+console.error("Error!");
