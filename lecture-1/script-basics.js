04c52eb523edf399276c901f9bcfad4511763297
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2017-12-27 10:09:31 +0300

    lecture 1 works continues

--- lecture-1/script-basics.js
+++ lecture-1/script-basics.js
@@ -10,6 +10,3 @@ var div = document.createElement('div');
 document.querySelector('body').appendChild(div);
 document.querySelector('div').appendChild(script);
 
-// var scriptElem = document.createElement('script');
-// elem.innerHTML = 'alert("hello world")';
-// document.querySelector('head').appendChild(elem);
