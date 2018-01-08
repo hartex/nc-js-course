bb8a12bb6a4f396923ba754815ddfaf51f2e8e30
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-01-09 00:00:29 +0300

    removed unnecessary method invocation

--- lecture-1/3-script-loading-basics/script-basics.js
+++ lecture-1/3-script-loading-basics/script-basics.js
@@ -4,4 +4,3 @@
 var script = document.createElement('script');
 script.innerHTML = 'alert("hello world")';
 document.querySelector('body').appendChild(script);
-document.querySelector('head').appendChild(script);
