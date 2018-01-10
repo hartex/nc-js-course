eef2cc95bd8f1b60f7d237e9cfaeaf14656b4bef
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-01-10 10:59:16 +0300

    start working on 5 chapter

---
+++ lecture-1/5-language-introduction/language-introduction.js
@@ -0,0 +1,24 @@
+/* 
+statement; 
+statement;
+statement
+statement
+ */
+
+/*
+block statement: 
+{
+    statement; 
+    statement;
+}
+*/
+
+/**
+ * Variable declaration and assignment
+ * */
+
+var x = 1;
+
+let y = 2;
+
+const z = 3;
