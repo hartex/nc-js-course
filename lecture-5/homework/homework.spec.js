98f85b8ebc4f70dedb57574e0038b6feb092acdf
Nikolay Kluyev <nikolay.klyuev@netcracker.com>
2018-02-02 19:58:52 +0300

    added test for homework #5

--- lecture-5/homework/homework.spec.js
+++ lecture-5/homework/homework.spec.js
@@ -6,6 +6,12 @@ beforeEach(() => {
     setUpList();
 });
 
+function setUpList() {
+    list = new LinkedList(1, 23, 44, 'dsfs', {});
+}
+
+/* --- actual tests --- */
+
 test('List length should be correct', () => {
     expect(list.length).toBe(5);
 });
@@ -24,7 +30,33 @@ test('List push() adds element to the list tail', () => {
     expect(list.get(5)).toBe('dsdf');
 });
 
+test('List unshift() adds one or more elements at the beginning if the list', () => {
+    list.unshift('sdd', 222);
+    expect(list.get(0)).toBe('sdd');
+    expect(list.get(1)).toBe(222);
+});
 
-function setUpList() {
-    list = new LinkedList(1, 23, 44, 'dsfs', {});
-}
+test('List pop() removes one element from the end of the list and returns it', () => {
+    const lastElement = list.pop();
+    expect(list.length).toBe(4);
+    expect(lastElement).toBe({});
+});
+
+test('List shift() removes one element from the beginning of the list and returns it', () => {
+    const firstElement = list.shift();
+    expect(list.length).toBe(4);
+    expect(firstElement).toBe(1);
+});
+
+test('List contains() checks if an element exist in the list', () => {
+    expect(list.contains(1)).toBe(true);
+    expect(list.contains('asdfasdf')).toBe(false);
+});
+
+test('List contains() returns correct string representation of the list', () => {
+    expect(list.toString()).toBe('[1, 23, 44, "dsfs", {}]');
+});
+
+test('List reverse() returns correct string representation of the list', () => {
+    expect(list.reverse().toString()).toBe('[{}, "dsfs", 44, 23, 1]');
+});
