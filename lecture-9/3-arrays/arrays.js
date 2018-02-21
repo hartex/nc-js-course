68c2516f8363cb3ffe591a452985d3b338c308f8
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-02-21 18:13:19 +0300

    lecture 9: finalized

--- lecture-9/3-arrays/arrays.js
+++ lecture-9/3-arrays/arrays.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Comment } from '../2-handling-events/Comment';
 
+const commentsData = {
+  text: 'some initial comment text',
+  date: '22.12.2013: 12:33',
+  user: {
+    name: 'harold',
+    avatar: 'https://naked-science.ru/sites/default/files/field/image/maxresdefault_60.jpg'
+  }
+}
 
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li key={number.toString()}>{number}</li>
+);
 
-ReactDOM.render(<Clock />, document.getElementById('app-root'));
+const App = (
+  [
+    <ul key="elements">{listItems}</ul>,
+    <div key="components">
+      {/* note that keys is not being passed as props */}
+      {numbers.map((v) => <Comment key={v} comment={commentsData} />)}
+    </div>
+  ]
+);
+
+ReactDOM.render(App, document.getElementById('app-root'));
